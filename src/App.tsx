05c95be134c0a754e3efdf02aff7bb615d51f2ce
Aman Pandey <skytechno00@gmail.com>
2025-04-04 18:39:29 +0530

    strip responsive

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <Topstrip />
-      <Header />
+      {/* <Header /> */}
       <ScrollToTop />
       <Routes>
         <Route path='/' element={<HomeView />} />
@@ -27,7 +27,7 @@ function App() {
         <Route path="/blog/:slug" element={<BlogView />} />
         <Route path='/contact' element={<ContactView />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
--- src/components/common/Topstrip/index.tsx
+++ src/components/common/Topstrip/index.tsx
@@ -10,7 +10,7 @@ const Topstrip = () => {
             |
             <div className="flex gap-1">
                 <a className={`${styles.stripfont}`} href={`tel:${ContactData.phone1}`}>+91-{ContactData.phone1},</a>
-                <a className={`${styles.stripfont}`} href={`tel:${ContactData.phone2}`}>+91-{ContactData.phone2}</a>
+                <a className={`${styles.stripfont} ${styles.phone2}`} href={`tel:${ContactData.phone2}`}>+91-{ContactData.phone2}</a>
             </div>
         </div>)
 }
