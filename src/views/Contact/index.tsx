e9f18016d76469f336571e6ca8099a1192704280
Aman Pandey <skytechno00@gmail.com>
2025-04-05 12:40:48 +0530

    contact view done

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -46,10 +46,9 @@ const ContactView = () => {
                 <iframe
                     className={`${styles.map}`}
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.2698770932325!2d77.19160719999999!3d28.6516369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d029c5f769541%3A0xd28083094a50b672!2sHigh%20Tech%20Window%20Blinds%20-%20Vishal%20Sales%20Corporation!5e0!3m2!1sen!2sin!4v1743678610078!5m2!1sen!2sin"
-                    width="1200" height="400"
+                    width="100%" height="400"
                     loading="lazy"
                 >
-
                 </iframe>
             </div>
         </div>
