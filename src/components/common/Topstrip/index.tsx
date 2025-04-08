e37b447fb19fbb30c298dc12a142cf277e45a052
Aman Pandey <skytechno00@gmail.com>
2025-04-08 12:19:22 +0530

    font changed

--- src/components/common/Topstrip/index.tsx
+++ src/components/common/Topstrip/index.tsx
@@ -8,7 +8,7 @@ const Topstrip = () => {
                 <a className={`${styles.stripfont}`} href={`mailto:${ContactData.email}`}>{ContactData.email}</a>
             </div>
             |
-            <div className="flex gap-1">
+            <div className={`${styles.phoneContainer}`}>
                 <a className={`${styles.stripfont}`} href={`tel:${ContactData.phone1}`}>+91-{ContactData.phone1},</a>
                 <a className={`${styles.stripfont} ${styles.phone2}`} href={`tel:${ContactData.phone2}`}>+91-{ContactData.phone2}</a>
             </div>
