feca5d6447a7cfe62c93a834ac835413b4842b5c
Aman Pandey <skytechno00@gmail.com>
2025-04-10 13:57:32 +0530

    seo set up done

--- src/components/common/PageSeo/index.tsx
+++ src/components/common/PageSeo/index.tsx
@@ -21,12 +21,7 @@ const PageSeo = ({
             {image && <meta property="og:image" content={image} />}
             {url && <meta property="og:url" content={url} />}
             <meta property="og:description" content={description} />
-            {/* twitter meta tags */}
-            <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@feelsafe_mark1" />
-            <meta property="og:site_name" content="feelsafemarkone
-            
-            "></meta>
+
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={image} />
